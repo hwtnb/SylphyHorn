e1cf87adb05e303f57527f077965d2903f020bbb
HaijinW <hiroki@valeria-journal.net>
2019-07-02 14:55:27 +0900

    Add the rest of mouse shortcuts. And fix mixed line endings.

--- source/SylphyHorn.Core/Serialization/MouseShortcutSettings.cs
+++ source/SylphyHorn.Core/Serialization/MouseShortcutSettings.cs
@@ -11,6 +11,21 @@ namespace SylphyHorn.Serialization
 		public MouseShortcutSettings(ISerializationProvider provider)
 			: base(provider)
 		{
+			ValidateCode(MoveLeftAndSwitch);
+			ValidateCode(MoveRightAndSwitch);
+			ValidateCode(MoveNewAndSwitch);
+			ValidateCode(TogglePin);
+		}
+
+		private void ValidateCode(ShortcutkeyProperty property)
+		{
+			if (!property.Value.All(code => IsValidCode(code))) property.Value = null;
+		}
+
+		private bool IsValidCode(int code)
+		{
+			// (LButton <= code && code <= XButton2 && code != Cancel) || (WheelDown <= code && code < WheelUp)
+			return (0 < code && code < 7 && code != 3) || (262144 * 2 < code && code < 262144 * 2 + 3);
 		}
 	}
 }
