cd7ea5f89042b2b1aaf72c8154d600b181d71b98
hwtnb <hiroki.w@gmail.com>
2022-02-26 15:41:25 +0900

    Bump version.

--- source/SylphyHorn/Properties/AssemblyInfo.cs
+++ source/SylphyHorn/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyMetadata("ExtraVersion", "beta.4")]
+[assembly: AssemblyMetadata("ExtraVersion", "beta.5")]
 [assembly: ComVisible(false)]
