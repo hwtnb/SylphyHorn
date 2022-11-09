b6230b2ad4d658eb75be333a1dc78da685eb6a90
hwtnb <hiroki.w@gmail.com>
2022-11-09 23:24:32 +0900

    Convert projects to SDK-style.

--- source/SylphyHorn/Properties/AssemblyInfo.cs
+++ source/SylphyHorn/Properties/AssemblyInfo.cs
@@ -2,17 +2,6 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
-[assembly: AssemblyTitle("SylphyHornPlus")]
-[assembly: AssemblyDescription("Virtual Desktop Tools for Windows 11 and 10")]
-[assembly: AssemblyCompany("hwtnb.net")]
-[assembly: AssemblyProduct("SylphyHornPlus")]
-[assembly: AssemblyCopyright("Copyright © 2015 - 2018 Manato KAMEYA")]
 [assembly: Guid("20C87E1A-AF2A-486F-84BF-7D4FD19CF716")]
-
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyMetadata("ExtraVersion", "beta.7")]
-[assembly: AssemblyMetadata("OriginalCompany", "grabacr.net")]
-[assembly: AssemblyMetadata("OriginalProduct", "SylphyHorn")]
 [assembly: ComVisible(false)]
